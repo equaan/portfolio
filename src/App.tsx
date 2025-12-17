d208349b6f8cfc1c0251edbfa246fdff6fb7d997
Mohammad Equaan Kacchi <equaankacchi367@gmail.com>
2025-12-17 11:41:59 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,8 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      {/* CHANGE HERE: We added the basename prop */}
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
