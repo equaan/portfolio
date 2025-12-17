2347ae41cf5bb047f57ed41b506b0d385419be6d
Mohammad Equaan Kacchi <equaankacchi367@gmail.com>
2025-12-17 11:36:42 +0530

    Set base path for portfolio in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,10 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  // START OF CHANGE: Add your repository name here
+  base: "/portfolio/", 
+  // END OF CHANGE
+  
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
